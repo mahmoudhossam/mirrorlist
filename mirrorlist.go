85e2a6641a85eb61444143eaaa15a6a1391e36eb
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-05-11 06:17:09 +0200

    Added output flag

--- mirrorlist.go
+++ mirrorlist.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func get_mirrors() []byte {
+func GetMirrors() []byte {
 	response, err := http.Get("https://www.archlinux.org/mirrors/status/json/")
 	if err != nil {
 		log.Fatal(err)
 	}
 	read, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
+	defer response.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,13 +34,32 @@ type Result struct {
 	Urls []Mirror
 }
 
+func SetOut(f string) (w io.Writer) {
+	if strings.TrimSpace(f) == "" {
+		w = os.Stdout
+	} else {
+		file, err := os.Create(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w = file
+	}
+	return
+}
+
 func main() {
-	mirrors := get_mirrors()
+	var f string
+	flag.StringVar(&f, "o", "", "Output to file.")
+	flag.Parse()
+	out := SetOut(f)
+	mirrors := GetMirrors()
 	var result Result
 	err := json.Unmarshal(mirrors, &result)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println(len(result.Urls))
+	}
+	for _, v := range result.Urls {
+		fmt.Fprintf(out, "## Score: %v, %v\n", v.Score, v.Country)
+		fmt.Fprintf(out, "#Server = %v$repo/os/$arch\n\n", v.Url)
 	}
 }
